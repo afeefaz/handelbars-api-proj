9eeee1109bcdf841cfbf762aeb191298b35aab0c
Afeef <afeef.as11@gmail.com>
2021-05-05 14:40:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const update = function(){
+   let num = $('#input').val() || 5
+   const apiURL = 'https://randomuser.me/api/?results='+num
+
+   $.get(apiURL, function( data ) {
+    let source = $('#user-template').html();
+    let template = Handlebars.compile(source);
+    
+    let newHTML = template(data);
+    $('.container').append(newHTML);
+  });
+}
+
+update()
